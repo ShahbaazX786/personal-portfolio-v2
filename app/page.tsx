c957145b93b5bb4e33b2e51f49b3ad4319ab3cf4
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-01-13 15:19:49 +0530

    Updated Homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Navbar from "@/components/layout/Navbar";
+
 export default function Home() {
-  return <h1 className="flex-1 text-center bg-green-500">Bismillah</h1>;
+  return (
+    <>
+      {/* <h1 className="flex-1 text-center bg-green-500">Bismillah</h1>; */}
+      <Navbar />
+    </>
+  );
 }
