3dc955431f382804675648228271e7d8b4cc1b94
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-01-13 17:00:13 +0530

    Added a welcome page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/layout/Navbar";
+import Welcome from "@/components/Welcome";
 
 export default function Home() {
   return (
     <>
       {/* <h1 className="flex-1 text-center bg-green-500">Bismillah</h1>; */}
       <Navbar />
+      <Welcome />
     </>
   );
 }
