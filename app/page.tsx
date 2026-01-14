1fd973bbca730115e03a85d3bd12cece3cd5632e
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-01-14 16:39:53 +0530

    Added resume window in main page.

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import BottomDock from "@/components/layout/BottomDock";
 import Navbar from "@/components/layout/Navbar";
 import Welcome from "@/components/Welcome";
 import CommandCLI from "@/components/windows/cmd";
+import ResumeWindow from "@/components/windows/Resume";
 import SafariWindow from "@/components/windows/SafariBrowser";
 import gsap from "gsap";
 import { Draggable } from "gsap/all";
@@ -20,6 +21,7 @@ export default function Home() {
 
       <CommandCLI />
       <SafariWindow />
+      <ResumeWindow />
     </main>
   );
 }
