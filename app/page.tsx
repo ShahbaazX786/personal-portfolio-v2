672fb47f1f8059f165e54c58b8a0a958278d2db9
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-01-14 15:35:09 +0530

    Updated main page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
+"use client";
+
 import BottomDock from "@/components/layout/BottomDock";
 import Navbar from "@/components/layout/Navbar";
 import Welcome from "@/components/Welcome";
+import CommandCLI from "@/components/windows/cmd";
+import gsap from "gsap";
+import { Draggable } from "gsap/all";
+
+gsap.registerPlugin(Draggable);
 
 export default function Home() {
   return (
-    <>
+    <main>
       {/* <h1 className="flex-1 text-center bg-green-500">Bismillah</h1>; */}
       <Navbar />
       <Welcome />
       <BottomDock />
-    </>
+
+      <CommandCLI />
+    </main>
   );
 }
