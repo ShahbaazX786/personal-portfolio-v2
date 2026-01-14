a5014510304898b74c903cee2cbc5919026226e1
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-01-14 14:26:18 +0530

    Updated constants.

--- lib/constants/constants.ts
+++ lib/constants/constants.ts
@@ -78,4 +78,17 @@ const WELCOME_PAGE_FONT_WEIGHTS: any = {
   title: { min: 400, max: 900, default: 400 },
 } as const;
 
-export { WELCOME_PAGE_FONT_WEIGHTS };
+const INITIAL_Z_INDEX = 1000;
+
+const WINDOW_CONFIG = {
+  finder: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+  contact: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+  resume: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+  safari: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+  photos: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+  terminal: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+  txtfile: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+  imgfile: { isOpen: false, zIndex: INITIAL_Z_INDEX, data: null },
+} as const;
+
+export { WELCOME_PAGE_FONT_WEIGHTS, WINDOW_CONFIG, INITIAL_Z_INDEX };
