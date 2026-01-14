43464acf91db2a32a01e162f4c01fb0ca34408e5
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-01-14 13:43:24 +0530

    Added gsap animations to the dock icons.

--- components/layout/BottomDock.tsx
+++ components/layout/BottomDock.tsx
@@ -1,15 +1,67 @@
 "use client";
 
 import { DockApps } from "@/lib/constants/constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import Image from "next/image";
 import { useRef } from "react";
-
 import { Tooltip } from "react-tooltip";
 
 const BottomDock = () => {
-  const dockRef = useRef(null);
+  const dockRef = useRef<null | HTMLDivElement>(null);
 
-  const toggleApp = (app) => {
+  useGSAP(() => {
+    const dock = dockRef.current;
+
+    if (!dock) return;
+
+    const icons = dock.querySelectorAll(".dock-icon");
+
+    const animateIcons = (mouseX: number) => {
+      const { left } = dock.getBoundingClientRect();
+
+      icons.forEach((icon) => {
+        const { left: iconLeft, width } = icon.getBoundingClientRect();
+        const center = iconLeft - left + width / 2;
+        const distance = Math.abs(mouseX - center);
+        const intensity = Math.exp(-(distance ** 2.5) / 20000);
+
+        gsap.to(icon, {
+          scale: 1 + 0.25 * intensity,
+          y: -15 * intensity,
+          duration: 0.2,
+          ease: "power1.out",
+        });
+      });
+    };
+
+    const handleMouseMove = (e: MouseEvent) => {
+      const { left } = dock.getBoundingClientRect();
+
+      animateIcons(e.clientX - left);
+    };
+
+    const resetIcons = () => {
+      icons.forEach((icon) =>
+        gsap.to(icon, {
+          scale: 1,
+          y: 0,
+          duration: 0.3,
+          ease: "power1.out",
+        })
+      );
+    };
+
+    dock.addEventListener("mousemove", handleMouseMove);
+    dock.addEventListener("mouseleave", resetIcons);
+
+    return () => {
+      dock.removeEventListener("mousemove", handleMouseMove);
+      dock.removeEventListener("mouseleave", resetIcons);
+    };
+  }, []);
+
+  const toggleApp = (app: { id: string; canOpen: boolean }) => {
     console.log(app);
   };
 
