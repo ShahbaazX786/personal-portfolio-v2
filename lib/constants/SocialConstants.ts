8eea943adcaa7f88a10994e7ed82481a510e4480
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-01-15 19:38:50 +0530

    Added social constants.

---
+++ lib/constants/SocialConstants.ts
@@ -0,0 +1,30 @@
+export const socials = [
+  {
+    id: 1,
+    text: "Github",
+    icon: "/icons/github.svg",
+    bg: "#f4656b",
+    link: "https://github.com/ShahbaazX786",
+  },
+  {
+    id: 2,
+    text: "Blogs",
+    icon: "/icons/atom.svg",
+    bg: "#4bcb63",
+    link: "https://dev.to/shahbaazx786",
+  },
+  {
+    id: 3,
+    text: "Twitter/X",
+    icon: "/icons/twitter.svg",
+    bg: "#ff866b",
+    link: "emailto:shahbaazalam78@gmail.com",
+  },
+  {
+    id: 4,
+    text: "LinkedIn",
+    icon: "/icons/linkedin.svg",
+    bg: "#05b6f6",
+    link: "https://www.linkedin.com/in/shaik-shahbaaz-alam",
+  },
+] as const;
