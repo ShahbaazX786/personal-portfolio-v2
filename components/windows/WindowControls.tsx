119d87ed57c0da32a581a4cdeea83eabf5450861
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-01-16 17:48:03 +0530

    Added optional icons view in window controls.

--- components/windows/WindowControls.tsx
+++ components/windows/WindowControls.tsx
@@ -1,14 +1,41 @@
 import { WindowKey } from "@/store/types/window.store.type";
 import useWindowStore from "@/store/window.store";
+import clsx from "clsx";
+import { Minus, Square, X } from "lucide-react";
 
-const WindowControls = ({ target }: { target: WindowKey }) => {
+const WindowControls = ({
+  target,
+  icons = false,
+}: {
+  target: WindowKey;
+  icons?: boolean;
+}) => {
   const { closeWindow } = useWindowStore();
 
   return (
     <div id="window-controls">
-      <button className="close" onClick={() => closeWindow(target)} />
-      <button className="minimize" />
-      <button className="maximize" />
+      <button
+        className={clsx("close", icons && "flex justify-center items-center")}
+        onClick={() => closeWindow(target)}
+      >
+        {icons && <X className="w-3 text-white" />}
+      </button>
+      <button
+        className={clsx(
+          "minimize",
+          icons && "flex justify-center items-center"
+        )}
+      >
+        {icons && <Square className="w-2 text-white" />}
+      </button>
+      <button
+        className={clsx(
+          "maximize",
+          icons && "flex justify-center items-center"
+        )}
+      >
+        {icons && <Minus className="w-2 text-white" />}
+      </button>
     </div>
   );
 };
