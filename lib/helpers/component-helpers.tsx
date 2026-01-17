5b11a9e80f5396d4581566c5381f05abc210c5b8
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-01-17 16:12:47 +0530

    Moved renderText to a reusable component.

---
+++ lib/helpers/component-helpers.tsx
@@ -0,0 +1,13 @@
+const renderText = (text: string, className: string, baseWeight = 400) => {
+  return [...text].map((char, i) => (
+    <span
+      key={i}
+      className={className}
+      style={{ fontVariationSettings: `'wght' ${baseWeight}` }}
+    >
+      {char === " " ? "\u00A0" : char}
+    </span>
+  ));
+};
+
+export { renderText };
