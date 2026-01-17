0be4f54c0415692c6b0e53db19300c9265dea083
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-01-17 14:51:33 +0530

    Updated Navbar time with calender component.

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -5,6 +5,8 @@ import { WindowKey } from "@/store/types/window.store.type";
 import useWindowStore from "@/store/window.store";
 import dayjs from "dayjs";
 import Image from "next/image";
+import { Calendar } from "../ui/calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 const Navbar = () => {
   const { openWindow } = useWindowStore();
@@ -36,18 +38,41 @@ const Navbar = () => {
       <div>
         <ul>
           {NavIcons.map(({ id, img }) => (
-            <li key={id}>
-              <Image
-                src={img}
-                className="size-4 icon-hover"
-                alt={`icon-${id}`}
-                width={100}
-                height={100}
-              />
-            </li>
+            <>
+              <li key={id}>
+                <Image
+                  src={img}
+                  className="size-4 icon-hover"
+                  alt={`icon-${id}`}
+                  width={100}
+                  height={100}
+                />
+              </li>
+            </>
           ))}
+          <li>
+            <Popover>
+              <PopoverTrigger
+                asChild
+                className="hover:scale-110 transition-all ease-in-out"
+              >
+                <time className="cursor-pointer">
+                  {dayjs().format("ddd MMM D h:mm A")}
+                </time>
+              </PopoverTrigger>
+              <PopoverContent
+                className="w-auto overflow-hidden p-0"
+                align="start"
+              >
+                <Calendar
+                  mode="single"
+                  selected={new Date()}
+                  captionLayout="dropdown"
+                />
+              </PopoverContent>
+            </Popover>
+          </li>
         </ul>
-        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
       </div>
     </nav>
   );
