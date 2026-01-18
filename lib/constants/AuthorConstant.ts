c1a6e634df7d4bee532126a75b40dc062f0f9619
shahbaazx786 <shahbaazplatinum@gmail.com>
2026-01-18 12:33:20 +0530

    Added author constants.

---
+++ lib/constants/AuthorConstant.ts
@@ -0,0 +1,5 @@
+export const AuthorDetails = {
+  name: "Shaik Mohammad Shahbaaz Alam",
+  title: "Full Stack Engineer",
+  other_titles: ["Frontend Developer, Backend Developer, Full Stack Developer"],
+};
